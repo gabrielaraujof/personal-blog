0655d2240f59fc13956d9d9f2f19980f1028d459
Gabriel Araujo <git@gabrielaraujo.dev>
2020-12-05 23:12:39 -0300

    fix: a11y issues

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -19,23 +19,23 @@ const Post = ({ post }: Props) => {
   const { tags, title, date } = post.frontmatter;
 
   return (
-    <div className={styles['post']}>
+    <article className={styles['post']}>
       <Link className={styles['post__home-button']} to="/">All Articles</Link>
 
       <div className={styles['post__content']}>
         <Content body={html} title={title} />
       </div>
 
-      <div className={styles['post__footer']}>
+      <footer className={styles['post__footer']}>
         <Meta date={date} />
         {tags && tagSlugs && <Tags tags={tags} tagSlugs={tagSlugs} />}
         <Author />
-      </div>
+      </footer>
 
-      <div className={styles['post__comments']}>
+      <section className={styles['post__comments']}>
         <Comments postSlug={slug} postTitle={post.frontmatter.title} />
-      </div>
-    </div>
+      </section>
+    </article>
   );
 };
 
