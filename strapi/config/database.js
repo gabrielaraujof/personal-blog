125a165b613c2efb80e3fe741c6647551276672e
Gabriel Araujo <git@gabrielaraujo.dev>
2025-08-24 21:33:43 -0300

    chore(migration): initialize minimal Strapi project with content-types for posts and pages

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: env('DATABASE_FILENAME', '.tmp/data.db')
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db'))
     },
     useNullAsDefault: true
   }
